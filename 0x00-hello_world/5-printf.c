b36c2030c40e735216455eea32bda92053ece1fa
Jacob5696 <jacob.harawa1@gmail.com>
2023-06-18 09:52:54 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - will print out words using printf
+ *
+ * Return: should return 0
+ */
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
