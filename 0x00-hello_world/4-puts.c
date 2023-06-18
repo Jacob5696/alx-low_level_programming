ca42b868aae472b87c24c3a94b5390e83b5d316a
Jacob5696 <jacob.harawa1@gmail.com>
2023-06-18 09:36:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - a program to printout some words
  *
- * Return - 0
- * */
+ * Return - return 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
