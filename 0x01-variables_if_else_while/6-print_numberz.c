c65eecc796e0c7f2922660945a163695308fef60
Jacob5696 <jacob.harawa1@gmail.com>
2023-06-19 13:01:02 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the program prints numbers from 0 to 9.
+ *
+ * Return: should return 0
+ */
+int main(void)
+{
+	int l;
+
+	for (l = 48; l < 58; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
